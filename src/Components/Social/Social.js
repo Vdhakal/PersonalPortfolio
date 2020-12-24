82b9043e3cfdfb574e5a075fb8276947153df2d0
Bijay Poudel <bijay.r.poudel@gmail.com>
2020-12-23 22:37:09 -0600

    social path new tab

--- src/Components/Social/Social.js
+++ src/Components/Social/Social.js
@@ -7,10 +7,10 @@ const Social = () => {
             <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" />
             <nav className="social" >
                 <ul>
-                    <li><a href="https://twitter.com/gian_michelle">Twitter <i class="fa fa-twitter"></i></a></li>
-                    <li><a href="https://facebook.com/">Facebook <i class="fa fa-facebook"></i></a></li>
-                    <li><a href="http://dribbble.com/gian_michelle">LinkedIn <i class="fa fa-linkedin"></i></a></li>
-                    <li><a href="http://behance.net/gianm">Instagram <i class="fa fa-instagram"></i></a></li>
+                    <li><a href="https://twitter.com/" target="_blank">Twitter <i class="fa fa-twitter"></i></a></li>
+                    <li><a href="https://facebook.com/" target="_blank">Facebook <i class="fa fa-facebook"></i></a></li>
+                    <li><a href="http://linkedin.com/" target="_blank">LinkedIn <i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="http://instagram.com/" target="_blank">Instagram <i class="fa fa-instagram"></i></a></li>
                 </ul>
             </nav>
        </>
