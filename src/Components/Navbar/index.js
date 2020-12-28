d77b326b569d160a34e9042965dd3db88dd3a5ed
Bijay Poudel <bijay.r.poudel@gmail.com>
2020-12-27 22:24:38 -0600

    navbar tmro ma test gara ta

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,36 +1,60 @@
 import React from 'react';
-import {FaBars} from 'react-icons/fa';
+// import {FaBars} from 'react-icons/fa';
 import {
-    Nav, 
-    NavbarContainer, 
-    NavLogo, 
-    MobileIcon, 
-    NavMenu, 
-    Navitem,
+    // Nav, 
+    // NavbarContainer, 
+    // NavLogo, 
+    // MobileIcon, 
+    // NavMenu, 
+    // Navitem,
     NavBtn,
     NavBtnLink} from './NavbarElements';
-import './Button.css';
+// import './Button.css';
+import './nav.css'
+import $ from 'jquery';
+
+
+var tabs = $('.tabs');
+var selector = $('.tabs').find('a').length;
+//var selector = $(".tabs").find(".selector");
+var activeItem = tabs.find('.active');
+var activeWidth = activeItem.innerWidth();
+$(".selector").css({
+  "left": activeItem.position.left + "px", 
+  "width": activeWidth + "px"
+});
+
+$(".tabs").on("click","a",function(e){
+  e.preventDefault();
+  $('.tabs a').removeClass("active");
+  $(this).addClass('active');
+  var activeWidth = $(this).innerWidth();
+  var itemPos = $(this).position();
+  $(".selector").css({
+    "left":itemPos.left + "px", 
+    "width": activeWidth + "px"
+  });
+});
+
+
+
+
 
 const Navbar = ({toggle}) => {
     return (
-        <>
-        <Nav>
-            <NavbarContainer>
-                <NavLogo to='/'>VD</NavLogo>
-                <MobileIcon onClick={toggle}><FaBars /></MobileIcon>
-                <NavMenu>
-                    <Navitem to ="about">About</Navitem>
-                    <Navitem to ="projects">Projects</Navitem>
-                    <Navitem to ="work">Work</Navitem>
-                    <Navitem to ="skills">Skills</Navitem>
-                    <Navitem to ="skills">Contact</Navitem>
-                </NavMenu>
-                <NavBtn>
-                    <NavBtnLink class = "button" to ="/resume">Resume</NavBtnLink>
-                </NavBtn>
-            </NavbarContainer>
-        </Nav>
-       </>
+        <div class="wrapper">
+       
+            <nav class="tabs">
+                <div class="selector"></div>
+                <a href="#" class="active">ABOUT</a>
+                <a href="#">PROJECTS</a>
+                <a href="#">WORK</a> 
+                <a href="#">SKILLS</a>
+            
+            </nav>
+       
+      </div>
+  
     )
 }
 
